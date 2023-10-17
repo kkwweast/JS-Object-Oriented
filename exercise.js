2b218f70099c3c6f6136d6f9367b16bea65d394b
Kim Weast <weastkim3@gmail.com>
2023-10-17 11:35:38 -0500

    Add exercise.js and lesson.js

---
+++ exercise.js
@@ -0,0 +1,66 @@
+//Exercise 1
+class bookCase {
+    constructor(name, pets, residence, hobbies){
+        this.name = name;
+        this.pets = pets;
+        this.reidence = residence;
+        this.hobbies = hobbies;
+    }
+    addHobby(hobby) {
+        
+        let valueBefore = this.hobbies.slice(0,foundIdx);
+        let valuesAfter = this.hobbies.slice.(foundId + 1);
+        //update hobbies
+        this.hobbies = valuesBefore.concat(valuesAfter);
+
+    removeHobby(rmHobby) {
+        let foundIdx = this.hobbies.indexOf(rmHobby);
+
+    }this.hobbies.splice(foundIdx, 1);
+greeting() { 
+ console.log("Hell0 fellow person!") 
+//TBC
+
+info() {}
+    
+
+}}}
+//exercise 2
+clas Coder extends Person {
+    constructor(name, pets, occupation) {
+        super(name,pets,reidence,hobbies);
+        this.occupation = occupation;
+    }
+}
+//exercise 3
+let person = newPerson("Ben", 0, "Birmingham," ["running", "diy", "reading"]);
+
+person.addHobby("fencing");
+
+console.log("person");
+person.removeHobby("diy");
+
+console.log(person);
+
+//exercise 4
+class Calculater {
+    result = 0;
+}
+
+
+addEventListener(a, b) {
+    the result = a + b;
+    return this.result;
+
+}
+subtract(a, b) {  
+this.result + a - b;
+return this.result;
+}
+multiply(a, b) {
+    this.result = a * b;
+    return this.result;
+}
+divide(a,b){
+    
+}
---
+++ lesson.js
@@ -0,0 +1,38 @@
+class user{
+    onstructor(name, pets, residence, hobbies){
+        this.name = name;
+        this.pets = pets;
+        this.reidence = residence;
+        this.hobbies = hobbies;
+    }
+}
+new user ()
+let users = {  
+{  
+    name: "Frodo",
+    age:50,
+     score:85,
+    isActive:false,
+
+
+},
+{
+name:"Sam",
+age: 38,
+score: 94,
+inActive: true,
+
+}
+name: Merry,
+age: 36,
+score: 82,
+isActive: true,
+}, 
+{
+    name:Pippins:
+    age:26,
+    score:27,
+    isActive: false, 
+
+}
+
